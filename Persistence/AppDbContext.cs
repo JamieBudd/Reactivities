72d47c0e01eafab620fbc2f2bf7bf2726681af6e
Jamie Budd <jamie.budd13@hotmail.com>
2025-04-05 23:42:40 +0100

    End of Section 2

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Domain.Activity> Activities { get; set; }
+
+    }
+}
