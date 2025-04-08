359cb40fe9ca006d7fff089fe4c1c5395a93f0d7
Jamie Budd <jamie.budd13@hotmail.com>
2025-04-08 23:35:27 +0100

    End of section 4

---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Domain;
+using System;
+
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Activity, Activity>();
+        }
+    }
+}
